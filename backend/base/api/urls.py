3958a6121401e941e6f3963a2242b9dd97dccee7
ethanny <145535621+ethanny@users.noreply.github.com>
2025-04-30 11:14:15 +0800

    fixed broken profile when signed in

--- backend/base/api/urls.py
+++ backend/base/api/urls.py
@@ -2,13 +2,14 @@ from rest_framework.routers import DefaultRouter
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import BookViewSet
-
+from . import views
 router = DefaultRouter()
 router.register(r'books', BookViewSet, basename='book')
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('profile/', views.get_profile),
 ]
 
 urlpatterns += router.urls
